c9d4da485bb87629cf8a09728b74ef3deedd10b0
aarwitz <horwina@bu.edu>
2025-12-06 11:11:39 -0500

    Correct interpretation of Actor-Critic outputs

--- rl_approach/network.py
+++ rl_approach/network.py
@@ -9,8 +9,9 @@ from torch.distributions.transforms import TanhTransform
 class ActorCritic(nn.Module):
     """
     Actor-Critic network for PPO.
-    Actor outputs continuous actions (steer, gas, brake).
-    Critic outputs state value, V(s).
+    - Actor outputs the mean and log-standard-deviation parameters of a Gaussian 
+    distribution over continuous actions (steer, gas, brake).
+    - Critic outputs state value, V(s).
     """
     def __init__(self, num_inputs=4, num_actions=3):
         super(ActorCritic, self).__init__()
